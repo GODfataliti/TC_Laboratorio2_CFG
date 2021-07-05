3df9423015690a91c8ab76392d1c8598f1200b49
Fataliti <hell.jnacho@gmail.com>
2021-07-04 20:56:47 -0400

    Automata Sudafrica: done.

--- 01.py
+++ 01.py
@@ -17,103 +17,116 @@ class Pila:
         return self.items
 
 
-
+#Automata.
 class Sudafrica:
 
     def __init__(self,arr):
         self.pila = Pila()
-        self.arr = list(arr)
+        self.arr = arr
         #e: no cambia la cadena, desapila.
 
     
     def q0(self):
         #Si esta vacio, apilo una Z.
-        if(len(self.pila)==0):
+        if(len(self.pila.items)==0):
             self.pila.apilar('Z')
-            self.q1()
+            return self.q1()
     
     def q1(self):
         
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #Si hay un A en el arr y una Z en la pila, apilo una A.
-        if(self.arr[0].upper()=='A' and self.pila[-1].upper()=='Z'):
+        if(self.arr[0].upper()=='A' and self.pila.items[-1].upper()=='Z'):
             self.arr.pop(0) #Borra el elemento visto del arr
             self.pila.apilar('A') #Agrega el elemento visto en el arr
-            self.q1()
+            return self.q1()
 
         #Si hay un A en el arr y una A en la pila, apilo una A.
-        if(self.arr[0].upper()=='A' and self.pila[-1].upper()=='A'):
+        elif(self.arr[0].upper()=='A' and self.pila.items[-1].upper()=='A'):
             self.arr.pop(0) #Borra el elemento visto del arr
             self.pila.apilar('A') #Agrega el elemento visto en el arr
-            self.q1()
+            return self.q1()
         
         #Si hay un C en el arr y una A en la pila, apilo una C.
-        if(self.arr[0].upper()=='C' and self.pila[-1].upper()=='A'):
+        elif(self.arr[0].upper()=='C' and self.pila.items[-1].upper()=='A'):
             self.arr.pop(0) #Borra el elemento visto del arr
             self.pila.apilar('C') #Agrega el elemento visto en el arr
-            self.q2()
+            return self.q2()
 
     
     def q2(self):
         
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #Si hay un C en el arr y una C en la pila, apilo una C.
-        if(self.arr[0].upper()=='C' and self.pila[-1].upper()=='C'):
+        if(self.arr[0].upper()=='C' and self.pila.items[-1].upper()=='C'):
             self.arr.pop(0)
             self.pila.apilar('C')
-            self.q2()
+            return self.q2()
         
         #Si hay un U en el arr y una C en la pila, desapila.
-        if(self.arr[0].upper()=='U' and self.pila[-1].upper()=='C'):
+        elif(self.arr[0].upper()=='U' and self.pila.items[-1].upper()=='C'):
             self.arr.pop(0)
             self.pila.desapilar()
-            self.q3()
+            return self.q3()
         
         #Si hay un G en el arr y una A en la pila, desapila.
-        if(self.arr[0].upper()=='G' and self.pila[-1].upper()=='A'):
+        elif(self.arr[0].upper()=='G' and self.pila.items[-1].upper()=='A'):
             self.arr.pop(0)
             self.pila.desapilar()
-            self.q4()
+            return self.q4()
     
     def q3(self):
         
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #No se modifica el arr, y si hay C en la pila, desapila.
-        if(self.pila[-1].upper()=='C'):
+        if(self.pila.items[-1].upper()=='C'):
             self.pila.desapilar()
-            self.q2()
+            return self.q2()
     
     def q4(self):
 
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #Si hay G en el arr, y una A en la pila, desapilo.
-        if(self.arr[0].upper()=='G' and self.pila[-1].upper()=='A'):
+        if(self.arr[0].upper()=='G' and self.pila.items[-1].upper()=='A'):
             self.pila.desapilar()
-            self.q4()
+            return self.q4()
         
         #Si hay una G en el arr, elimino la G.
-        if(self.arr[0].upper()=='G'):
+        elif(self.arr[0].upper()=='G'):
             self.arr.pop(0)
-            self.q5()
+            return self.q5()
     
     def q5(self):
 
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #Si hay una G en el arr, elimino la G.
-        if(self.arr[0].upper()=='G'):
-            self.arr.pop(0)
-            self.q5()
+        if(len(self.arr)>0):
+            if(self.arr[0].upper()=='G'):
+                self.arr.pop(0)
+                return self.q5()
         
         #Si esta vacio el arr, y hay una Z en la pila, desapilo.
-        if(len(self.arr)==0 and self.pila[-1].upper()=='Z'):
+        elif(len(self.arr)==0 and self.pila.items[-1].upper()=='Z'):
             self.pila.desapilar()
-            self.q6()
+            return self.q6()
     
     def q6(self):
+
+        print(f'Arr: {self.arr}')
+        print(f'Pila: {self.pila.items}')
         #Si tiene adn de negro.
         print("30cm confirmado.")
 
 
 
 
-def main():
 
-    Test = Sudafrica('AACCCCUUGGGGGG')
-
-if __name__ == '__main__':
-    main()
+arr = list('AACCCCUUGGGGGG')
+print(arr)
+Test = Sudafrica(arr)
+Test.q0()
